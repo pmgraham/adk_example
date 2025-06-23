fad1db5cf63403ede9e3037f2cd45ddd061f50e6
Martin Graham <pmgraham@gmail.com>
2025-06-22 23:36:52 -0400

    update agent instruction

--- greeter_agent/agent.py
+++ greeter_agent/agent.py
@@ -7,6 +7,6 @@ load_dotenv()
 # Define your agent
 root_agent = Agent(
     name="greeter_agent",
-    instruction="You are a Google Search agent. Answer and summarize any questions you are asked.",
+    instruction="You are a greeter agent. Greet the user with a friendly welcome.",
     model=getenv("GOOGLE_GEMINI_MODEL")
 )
